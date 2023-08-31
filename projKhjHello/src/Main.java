602ea38bd9dabdbbebcb59642ec6fe96c3178f45
Eippie8735 <119017270+Eippie8735@users.noreply.github.com>
2023-08-31 23:15:15 +0900

    my first Commit
    .

---
+++ projKhjHello/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        //커맨드 r 실행 / 컨트롤 d 디버그
+
+
+        System.out.println("my name is kcd!!");
+
+
+    }
+}
